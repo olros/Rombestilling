b7a03c8029ecd45ed0596b8ee3c170034f9882e9
Eirik Steira <eirsteir@stud.ntnu.no>
2021-05-12 16:40:25 +0200

    feat(users): users list, create and put (#22)
    * Add get all users endpoint
    
    * Add user builders and fix controller tests
    
    * Fix validation and remaining tests
    
    * Use type inference for one-line methods in user controller

--- backend/src/main/kotlin/ntnu/idatt2105/user/service/UserServiceImpl.kt
+++ backend/src/main/kotlin/ntnu/idatt2105/user/service/UserServiceImpl.kt
@@ -7,6 +7,8 @@ import ntnu.idatt2105.user.exception.UserNotFoundException
 import ntnu.idatt2105.user.model.User
 import ntnu.idatt2105.user.repository.UserRepository
 import org.modelmapper.ModelMapper
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Service
 import java.util.*
@@ -15,27 +17,26 @@ import java.util.*
 @Service
 class UserServiceImpl(val userRepository: UserRepository, val modelMapper: ModelMapper, val passwordEncoder: BCryptPasswordEncoder) : UserService {
 
-
-    private fun emailExist(email: String): Boolean {
-        return userRepository.findByEmail(email) != null
-    }
-    override fun createUser(user: UserRegistrationDto): UserDto {
-        if (emailExist(user.email)) {
+    override fun registerUser(user: UserRegistrationDto): UserDto {
+        if (existsByEmail(user.email))
             throw EmailInUseException()
-        }
+
         val userObj: User = modelMapper.map(user, User::class.java)
-        userObj.password = (passwordEncoder.encode(user.password))
         userObj.id = UUID.randomUUID()
+
         return modelMapper.map(userRepository.save(userObj), UserDto::class.java)
     }
 
-    override fun getUserDataByEmail(email: String): UserDto {
-        userRepository.findByEmail(email).run {
-            if(this != null){
-                return modelMapper.map(this, UserDto::class.java)
-            }
-            throw UserNotFoundException()
-        }
+    private fun existsByEmail(email: String): Boolean {
+        return userRepository.existsByEmail(email)
+    }
+
+    override fun getUsers(pageable: Pageable): Page<UserDto> =
+        userRepository.findAll(pageable).map { user -> modelMapper.map(user, UserDto::class.java) }
+
+    override fun getUser(id: UUID): UserDto {
+        val user = getUserById(id)
+        return modelMapper.map(user, UserDto::class.java)
     }
 
     override fun updateUser(id: UUID, user: UserDto): UserDto {
@@ -48,4 +49,8 @@ class UserServiceImpl(val userRepository: UserRepository, val modelMapper: Model
         )
         return modelMapper.map(userRepository.save(updatedUser), UserDto::class.java)
     }
+
+    private fun getUserById(id: UUID): User =
+        userRepository.findById(id).orElseThrow { UserNotFoundException() }
+
 }
---
+++ backend/src/test/kotlin/ntnu/idatt2105/user/controller/UserControllerTest.kt
@@ -0,0 +1,259 @@
+package ntnu.idatt2105.user.controller
+
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import io.github.serpro69.kfaker.Faker
+import ntnu.idatt2105.factories.UserFactory
+import ntnu.idatt2105.sercurity.config.JWTConfig
+import ntnu.idatt2105.user.dto.UserRegistrationDto
+import ntnu.idatt2105.user.model.User
+import ntnu.idatt2105.user.repository.UserRepository
+import ntnu.idatt2105.user.service.UserDetailsImplBuilder
+import org.hamcrest.Matchers.hasItem
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment
+import org.springframework.http.MediaType
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.test.context.support.WithMockUser
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import java.time.LocalDate
+import java.util.stream.Stream
+
+
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class UserControllerTest {
+    private val URI = "/users/"
+
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    @Autowired
+    private lateinit var objectMapper: ObjectMapper
+
+    @Autowired
+    private lateinit var jwtConfig: JWTConfig
+
+    @Autowired
+    private lateinit var userRepository: UserRepository
+
+    private lateinit var user: User
+    private val userFactory: UserFactory = UserFactory()
+    private lateinit var firstName: String
+    private lateinit var surname: String
+    private lateinit var birthDate: LocalDate
+    private lateinit var userDetails: UserDetails
+
+    /**
+     * Setting up variables that is the same for all tests
+     */
+    @BeforeEach
+    @Throws(Exception::class)
+    fun setUp() {
+        user = userFactory.getObject()
+        userRepository.save(user)
+        firstName = "Test"
+        surname = "Testersen"
+        birthDate = LocalDate.now()
+        userDetails = UserDetailsImplBuilder(id=user.id, email=user.email)
+            .build()
+    }
+
+    /**
+     * Cleans up the saved users after each test
+     */
+    @AfterEach
+    fun cleanUp() {
+        userRepository.deleteAll()
+    }
+
+    /**
+     * Test that you can create a user with valid input
+     *
+     * @throws Exception from post request
+     */
+    @ParameterizedTest
+    @MethodSource("provideValidEmails")
+    @Throws(Exception::class)
+    fun testCreateUserWithValidEmail(email: String) {
+        val validUser = UserRegistrationDto(
+            firstName=firstName,
+            surname=surname,
+            email=email,
+            phoneNumber=Faker().phoneNumber.phoneNumber()
+        )
+        mockMvc.perform(
+            post(URI)
+                .with(SecurityMockMvcRequestPostProcessors.csrf())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(validUser))
+        )
+            .andExpect(status().isCreated)
+            .andExpect(jsonPath("$.firstName").value(validUser.firstName))
+            .andExpect(jsonPath("$.data.password").doesNotExist())
+            .andExpect(jsonPath("$.data.email").doesNotExist())
+    }
+
+    @Test
+    @WithMockUser(value = "spring")
+    @Throws(Exception::class)
+    fun testGetUserByUserId() {
+        val testUser: User = userRepository.save(userFactory.getObject())
+        mockMvc.perform(
+            get(URI + testUser.id.toString() + "/")
+                .contentType(MediaType.APPLICATION_JSON).with(SecurityMockMvcRequestPostProcessors.csrf())
+        )
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("$.firstName").value(testUser.firstName))
+    }
+
+    @Test
+    @WithMockUser(value = "spring")
+    @Throws(Exception::class)
+    fun testGetAllUsers() {
+        mockMvc.perform(
+            get(URI)
+                .contentType(MediaType.APPLICATION_JSON).with(SecurityMockMvcRequestPostProcessors.csrf())
+        )
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("$.content.[*].firstName", hasItem(user.firstName)))
+    }
+
+    /**
+     * Test that a user can be created, but the same email cannot be used two times
+     *
+     * @throws Exception from post request
+     */
+    @Test
+    @Throws(Exception::class)
+    fun testCreateUserTwoTimesFails() {
+        val user: User = userFactory.getObject()
+        userRepository.save<User>(user)
+        val email: String = user.email
+
+        val validUser = UserRegistrationDto(
+            firstName=firstName,
+            surname=surname,
+            email=email,
+            phoneNumber=Faker().phoneNumber.phoneNumber()
+        )
+        mockMvc.perform(
+            post(URI)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(validUser))
+        )
+            .andExpect(status().isBadRequest)
+            .andExpect(
+                jsonPath("$.message").isNotEmpty
+            )
+    }
+
+    /**
+     * Test that a user cannot be created if email is on a wrong format
+     *
+     * @throws Exception
+     */
+    @ParameterizedTest
+    @MethodSource("provideInvalidEmails")
+    @Throws(Exception::class)
+    fun testCreateUserWithInvalidEmail(email: String) {
+        val invalidUser = UserRegistrationDto(
+            firstName=firstName,
+            surname=surname,
+            email=email,
+            phoneNumber=Faker().phoneNumber.phoneNumber()
+        )
+
+        mockMvc.perform(
+            post(URI)
+                .with(SecurityMockMvcRequestPostProcessors.csrf())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(invalidUser))
+        )
+            .andExpect(status().isBadRequest)
+            .andExpect(jsonPath("$.message").isNotEmpty)
+            .andExpect(jsonPath("$.data.email").exists())
+    }
+
+
+    /**
+     * Tests that get return a correct user according to token
+     *
+     * @throws Exception
+     */
+    @Test
+    @Throws(Exception::class)
+    fun testGetUserReturnsCorrectUser() {
+        mockMvc.perform(
+            get(URI + "me/")
+                .with(user(userDetails))
+        )
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("$.id").value(user.id.toString()))
+    }
+
+    /**
+     * Tests that put updated a user and returns the updated user info
+     *
+     * @throws Exception
+     */
+    @Test
+    @Throws(Exception::class)
+    fun testUpdateUserUpdatesUserAndReturnUpdatedData() {
+        user.surname = Faker().name.lastName()
+
+        mockMvc.perform(
+            put(URI + user.id.toString() + "/")
+                .with(user(userDetails))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(user))
+        )
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("$.id").value(user.id.toString()))
+    }
+
+    companion object {
+        /**
+         * Provides a stream of Valid emails to provide parameterized test
+         *
+         * @return Stream of valid emails
+         */
+        @JvmStatic
+        private fun provideValidEmails(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of("test123@mail.com"),
+                Arguments.of("test1.testesen@mail.com"),
+                Arguments.of("test_1234-testesen@mail.com")
+            )
+        }
+
+        /**
+         * Provides a stream of Invalid emails to provide parameterized test
+         *
+         * @return Stream of invalid emails
+         */
+        @JvmStatic
+        private fun provideInvalidEmails(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of("test123.no"),
+                Arguments.of("test@"),
+                Arguments.of("test@mail..com")
+            )
+        }
+    }
+}
