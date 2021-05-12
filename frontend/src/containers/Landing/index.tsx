cc98d2eb1f98c9af3ec6a5d6c1782ffe139f7c2e
Olaf Rosendahl <olafrosendahl@gmail.com>
2021-05-12 10:27:39 +0200

    chore(frontend): setup (#10)

---
+++ frontend/src/containers/Landing/index.tsx
@@ -0,0 +1,110 @@
+import Helmet from 'react-helmet';
+import { Link } from 'react-router-dom';
+import URLS from 'URLS';
+import { useIsAuthenticated } from 'hooks/User';
+
+// Material UI Components
+import { makeStyles, Typography, Button } from '@material-ui/core';
+
+// Project Components
+import Navigation from 'components/navigation/Navigation';
+import Logo from 'components/miscellaneous/Logo';
+import Container from 'components/layout/Container';
+
+const useStyles = makeStyles((theme) => ({
+  cover: {
+    position: 'relative',
+    color: theme.palette.common.white,
+    height: '100vh',
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  img: {
+    background: `${theme.palette.colors.gradient}`,
+    objectFit: 'contain',
+    boxShadow: 'inset 0 0 0 1000px rgba(0, 0, 0, 0.2)',
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    filter: 'blur(1px)',
+    zIndex: -1,
+  },
+  activityContainer: {
+    textAlign: 'center',
+    paddingTop: theme.spacing(2),
+  },
+  btnGroup: {
+    display: 'grid',
+    gap: theme.spacing(1),
+    paddingTop: theme.spacing(1),
+    gridTemplateColumns: 'auto auto',
+  },
+  button: {
+    color: theme.palette.common.white,
+    borderColor: theme.palette.common.white,
+  },
+  logoWrapper: {
+    display: 'flex',
+    margin: 'auto',
+    marginTop: theme.spacing(2),
+    maxWidth: 200,
+    maxHeight: 200,
+    marginBottom: theme.spacing(2),
+  },
+  logo: {
+    minWidth: '250px',
+    width: '46%',
+    maxWidth: '100%',
+    height: 'auto',
+    margin: theme.spacing(5, 'auto'),
+    [theme.breakpoints.down('md')]: {
+      minWidth: '200px',
+    },
+  },
+}));
+
+const Landing = () => {
+  const classes = useStyles();
+
+  return (
+    <Navigation maxWidth={false} topbarVariant={'transparent'}>
+      <Helmet>
+        <title>Forsiden - Rombestilling</title>
+      </Helmet>
+      <div className={classes.cover}>
+        <div className={classes.img} />
+        <div className={classes.logoWrapper}>
+          <Logo />
+        </div>
+        <Typography align='center' color='inherit' variant='h1'>
+          Rombestilling
+        </Typography>
+        <Typography align='center' color='inherit' variant='h3'>
+          Finn ditt rom nå!
+        </Typography>
+        {!useIsAuthenticated && (
+          <div className={classes.btnGroup}>
+            <Button className={classes.button} component={Link} to={URLS.LOGIN} variant='outlined'>
+              Logg inn
+            </Button>
+            <Button className={classes.button} component={Link} to={URLS.SIGNUP} variant='outlined'>
+              Registrer deg
+            </Button>
+          </div>
+        )}
+      </div>
+      <Container className={classes.activityContainer}>
+        <Typography gutterBottom variant='h1'>
+          Bestill rom
+        </Typography>
+      </Container>
+    </Navigation>
+  );
+};
+
+export default Landing;
---
+++ frontend/src/index.tsx
@@ -0,0 +1,120 @@
+import { ReactElement, ReactNode, useEffect, lazy, Suspense } from 'react';
+import ReactDOM from 'react-dom';
+import 'assets/css/index.css';
+import { useInterval } from 'hooks/Utils';
+import { getCookie } from 'api/cookie';
+import { ACCESS_TOKEN, REFRESH_TOKEN } from 'constant';
+import { StylesProvider } from '@material-ui/core';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AdapterDateFns from '@material-ui/lab/AdapterDateFns';
+import LocalizationProvider from '@material-ui/lab/LocalizationProvider';
+import { Navigate, BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import URLS from 'URLS';
+import 'delayed-scroll-restoration-polyfill';
+
+// Services
+import { ThemeProvider } from 'hooks/ThemeContext';
+import { SnackbarProvider } from 'hooks/Snackbar';
+import { useUser, useRefreshToken } from 'hooks/User';
+
+// Project components
+import Navigation from 'components/navigation/Navigation';
+
+// Project containers
+import Landing from 'containers/Landing';
+const Http404 = lazy(() => import('containers/Http404'));
+const Auth = lazy(() => import('containers/Auth'));
+const Profile = lazy(() => import('containers/Profile'));
+
+type AuthRouteProps = {
+  path: string;
+  element?: ReactElement | null;
+  children?: ReactNode;
+};
+
+const AuthRoute = ({ children, path, element }: AuthRouteProps) => {
+  const { data, isLoading } = useUser();
+
+  if (isLoading) {
+    return <Navigation isLoading noFooter />;
+  } else if (!data) {
+    return <Navigate to={URLS.LOGIN} />;
+  } else {
+    return (
+      <Route element={element} path={path}>
+        {children}
+      </Route>
+    );
+  }
+};
+
+type ProvidersProps = {
+  children: ReactNode;
+};
+
+export const Providers = ({ children }: ProvidersProps) => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 1000 * 60 * 2, // Don't refetch data before 2 min has passed
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
+  return (
+    <QueryClientProvider client={queryClient}>
+      <StylesProvider injectFirst>
+        <ThemeProvider>
+          <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <CssBaseline />
+            <SnackbarProvider>{children}</SnackbarProvider>
+          </LocalizationProvider>
+        </ThemeProvider>
+      </StylesProvider>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
+};
+
+const AppRoutes = () => {
+  const location = useLocation();
+  const refreshToken = useRefreshToken();
+  useEffect(() => {
+    window.gtag('event', 'page_view', {
+      page_location: window.location.href,
+      page_path: window.location.pathname,
+    });
+  }, [location]);
+  useInterval(() => {
+    const access_token = getCookie(ACCESS_TOKEN);
+    const refresh_token = getCookie(REFRESH_TOKEN);
+    if (!access_token && refresh_token) {
+      refreshToken.mutate(null);
+    }
+  }, 5000);
+  return (
+    <Routes>
+      <Route element={<Landing />} path={URLS.LANDING} />
+      <Route element={<Auth />} path={`${URLS.LOGIN}*`} />
+      <AuthRoute element={<Profile />} path={URLS.PROFILE} />
+
+      <Route element={<Http404 />} path='*' />
+    </Routes>
+  );
+};
+
+export const Application = () => {
+  return (
+    <Providers>
+      <BrowserRouter>
+        <Suspense fallback={<Navigation isLoading />}>
+          <AppRoutes />
+        </Suspense>
+      </BrowserRouter>
+    </Providers>
+  );
+};
+
+ReactDOM.render(<Application />, document.getElementById('root'));
