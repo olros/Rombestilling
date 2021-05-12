ac8e617f724ae12197f8b8e59782974b28f96f0d
Hermann <hermann.owren.elton@hotmail.com>
2021-05-12 17:33:58 +0200

    feat(section): PR Comments

--- backend/src/main/kotlin/ntnu/idatt2105/section/dto/SectionDto.kt
+++ backend/src/main/kotlin/ntnu/idatt2105/section/dto/SectionDto.kt
@@ -15,5 +15,5 @@ data class SectionDto(
         var description: String = "",
         var picture: String = "",
         var type: String = SectionType.ROOM,
-        var children: MutableList<SectionListDto> = mutableListOf()
+        var children: MutableList<SectionChildrenDto> = mutableListOf()
 )
