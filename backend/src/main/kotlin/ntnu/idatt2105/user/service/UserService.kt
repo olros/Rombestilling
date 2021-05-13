38ce24737aaf3f4faf458dc62f4e57ffcf332e3f
Mads Lundegaard <mads.lundegaard@live.no>
2021-05-13 09:11:31 +0200

    merged into role branch

--- backend/src/main/kotlin/ntnu/idatt2105/user/service/UserService.kt
+++ backend/src/main/kotlin/ntnu/idatt2105/user/service/UserService.kt
@@ -9,6 +9,7 @@ import java.util.*
 
 interface UserService {
     fun registerUser(user: UserRegistrationDto): UserDto
+    fun registerUserAsAdmin(user: UserRegistrationDto)
     fun updateUser(id: UUID, user: UserDto): UserDto
     fun getUsers(pageable: Pageable): Page<UserDto>
     fun getUser(id: UUID): UserDto
