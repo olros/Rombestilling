f1c56f1e0d956964ce19949c3bbec133ebd8ae55
Hermann <hermann.owren.elton@hotmail.com>
2021-05-19 09:12:05 +0200

    ..

--- backend/src/main/kotlin/ntnu/idatt2105/section/repository/SectionRepository.kt
+++ backend/src/main/kotlin/ntnu/idatt2105/section/repository/SectionRepository.kt
@@ -1,11 +1,8 @@
 package ntnu.idatt2105.section.repository
 
-import ntnu.idatt2105.reservation.model.QReservation
-import ntnu.idatt2105.reservation.model.Reservation
 import ntnu.idatt2105.section.model.QSection
 import ntnu.idatt2105.section.model.Section
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
 import org.springframework.data.querydsl.QuerydslPredicateExecutor
 import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer
 import org.springframework.data.querydsl.binding.QuerydslBindings
@@ -16,8 +13,8 @@ interface SectionRepository: JpaRepository<Section, UUID>, QuerydslPredicateExec
     @JvmDefault
     override fun customize(bindings: QuerydslBindings, section: QSection) {
         bindings.bind(section.name).first { path, value -> section.name.contains(value) }
-        bindings.bind(section.from).first { path, value -> !section.reservation.any().fromTime.after(value) }
-        bindings.bind(section.to).first { path, value -> !section.reservation.any().toTime.before(value) }
+        bindings.bind(section.from, section.to).first { path, value -> section.reservation.any().fromTime.before(value) }
+        bindings.bind(section.to).first { path, value ->!section.reservation.any().toTime.after(value) }
 
     }
 }
